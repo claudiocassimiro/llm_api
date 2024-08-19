fe1089272a39f80c4a10f0b45ec1acf8470e41fd
devclaudiocassimiro <claudio.silva@jala.university>
2024-08-19 08:16:10 -0300

    fix: hotfix

--- models.py
+++ models.py
@@ -2,6 +2,7 @@
 from utils.extensions import db, bcrypt
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
