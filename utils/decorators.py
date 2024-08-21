7f483da0197456925dbf303081a4687122f16308
Claudio Cassimiro <days_surfing@live.com>
2024-08-21 23:30:26 +0000

    refactor: add improvents on APIError class and require_api_key func

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,11 +1,21 @@
 from functools import wraps
 from flask import request, jsonify
 from models import User
+import logging
 
 class APIError(Exception):
     def __init__(self, message, status_code):
         super().__init__(message)
+        self.message = message
         self.status_code = status_code
+
+    def to_dict(self):
+        return {'error': self.message}
+
+    def handle(self):
+        response = jsonify(self.to_dict())
+        response.status_code = self.status_code
+        return response
 
 def require_api_key(view_function):
     @wraps(view_function)
@@ -16,5 +26,6 @@ def require_api_key(view_function):
             user = User.query.filter_by(api_key=api_key).first()
             if user:
                 return view_function(user, *args, **kwargs)
+        logging.error(f"Invalid or missing API key: {api_key}")
         raise APIError("API key inválida ou ausente", 401)
     return decorated_function
