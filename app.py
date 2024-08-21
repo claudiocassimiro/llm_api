9b402bb58a302e28f647e0d36722e36c7c55be68
Claudio Cassimiro <days_surfing@live.com>
2024-08-21 23:29:04 +0000

    refactor: add errorHandler

--- app.py
+++ app.py
@@ -2,8 +2,10 @@
 from flask import Flask
 from config import Config
 from utils.extensions import db, bcrypt
+from utils.decorators import APIError
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 
 db.init_app(app)
@@ -14,6 +16,10 @@ from routes import auth, llm, user
 app.register_blueprint(auth.bp)
 app.register_blueprint(llm.bp)
 app.register_blueprint(user.bp)
+
+@app.errorhandler(APIError)
+def handle_api_error(error):
+    return error.handle()
 
 if __name__ == '__main__':
     with app.app_context():
